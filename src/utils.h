6dc7118b359c44082c7548b029a714afa14aaeff
xavi075 <xm201532@gmail.com>
2023-11-06 08:55:50 +0100

    Deleted old files

--- src/utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef _AVRUTILS_H
-#define _AVRUTILS_H
-
-#define sbi(PORT, BITNO) (PORT |= 1 << BITNO)
-#define cbi(PORT, BITNO) (PORT &= ~(1 << BITNO))
-#define tbi(PORT, BITNO) (PORT ^= 1 << BITNO)
-
-#endif
