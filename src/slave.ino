5ef098b312f6c83dba22534d83f0d00de07d9d22
xavi075 <xm201532@gmail.com>
2023-11-07 12:23:55 +0100

    Add temperature implementation and lora read

--- src/slave.ino
+++ src/slave.ino
@@ -8,6 +8,7 @@
 // #include "LoRa.h"
 
 
+
 #include <Arduino.h>
 #include "modulator.h"
 // #include <avr/interrupt.h>
@@ -28,11 +29,20 @@
 #include <string.h>
 #include "gpio_device.h"
 
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
 #define N 128
 #define TEMPS 10
 
 
 volatile uint8_t value;
+// uint8_t temperatura;
+
+const int pin_temperatura = 14;
+OneWire oneWire(pin_temperatura);
+DallasTemperature sensors(&oneWire);
+
 static volatile float avg = 0;
 static volatile float avg_percent = 0;
 static uint8_t avg_str[10];
@@ -49,32 +59,18 @@ volatile uint8_t n = N-1;
 // typedef enum {regant, parat} estat_reg_t;
 // estat_reg_t estat_reg = parat;
 
-void floatToList(float num, uint8_t list[10]);
-// // void parse_lora( uint8_t * buf, uint8_t len, uint8_t status );
+void floatToList(float hum, float temp, uint8_t list[20]);
+void parse_lora( uint8_t * buf, uint8_t len, uint8_t status );
 void activa_lectura(void);
-void envia_humitat(void);
+void envia(void);
+void envia_resposta(void);
+void envia_temperatura(void);
 
 
 void setup(){
   // pin_t REGAR;
   // pin_t LED_R;
-  // int temps_2;
-  // temps_2 = 10;
   Serial.begin(9600);
-  // Serial.println("Hola");
-  // delay(10000);
-
-// LoRa cpp
-  // LoRa.setPins(10, 5, 2);
-  // LoRa.setSPIFrequency(8000000);
-  // Serial.println("Starting LoRa!");
-  // // LoRa.begin(866E6);
-  // while (!LoRa.begin(866E6)) {
-  //   Serial.println("Starting LoRa failed!");
-  //   delay(50000);
-  //   // while (1);
-  // }
-
   // adc
   // bona
   setup_ADC(1,5,16);//(adc_input,v_ref,adc_pre)
@@ -96,36 +92,47 @@ void setup(){
   //tmr2_pre 1,default=8,32,64,128,256,1024
   //TMR2=prescaler*(ocr2a+1)*T_clk
 
-  // init_stdout();
-  // serial_open();
-
   modulator_init();
   
   setup_tmr0(TEMPS, activa_lectura);
 
   // REGAR = pin_bind(&PORTD, 6, Output);
   // LED_R = pin_bind(&PORTD, 4, Output);
-  pinMode(4, OUTPUT);
+  pinMode(4, OUTPUT); //LED Blau
 
   Serial.println("Start lora init");
   while (!lora_init());
   Serial.println("Lora init passed");
 
+  sensors.begin();
+
+
   sei();
 }
 
-void floatToList(float num, uint8_t list[10]) {
-    char str[7];
-    dtostrf(num, 3, 2, str);
+void floatToList(float hum, float temp, uint8_t list[20]) {
+    char str_hum[7];
+    char str_temp[6];
+    dtostrf(hum, 3, 2, str_hum);
+    dtostrf(temp, 3, 1, str_temp);
     int i;
+    int j;
     list[0] = 'G';
     list[1] = '3';
     list[2] = ':';
     for (i = 0; i < 7; i++) {
-        if (i < strlen(str)) {
-            list[i+3] = str[i];
+        if (i < strlen(str_hum)) {
+            list[i+3] = str_hum[i];
         } else {
-            list[i+3] = '\0';
+            list[i+3] = '/';
+            break;
+        }
+    }
+    for (j = 0; j < 6; j++) {
+        if (j < strlen(str_temp)) {
+            list[j+i+4] = str_temp[j];
+        } else {
+            list[j+i+4] = '\0';
         }
     }
 }
@@ -133,17 +140,20 @@ void floatToList(float num, uint8_t list[10]) {
 void activa_lectura(void){
     tmr2_set(8);
     modulator_set(true);
-    // digitalWrite(4, HIGH);
-    // delay(1000);
-    // digitalWrite(4, LOW);
-    // delay(1000);    //
-    Serial.println("Timer 2");
-
 }
 
-void envia_humitat(void){
-    Serial.println(avg_percent);
-    lora_putd(avg_str, 10);
+void envia(void){
+  // pinMode(18, OUTPUT); //LED Blau
+  lora_putd(avg_str, 20);
+}
+
+void envia_resposta(void){
+  pinMode(18, OUTPUT); //LED Blau
+  lora_putd(avg_str, 20);
+}
+
+void envia_temperatura(void){
+    Serial.println("Prova");
 }
 
 // void envia_estat_bomba(void){
@@ -161,16 +171,25 @@ void envia_humitat(void){
 
 
 void loop(void){
-  Serial.println("Lexis is coming");
-  // register_lora_rx_event_callback( parse_lora );
+    register_lora_rx_event_callback( parse_lora );
+    // register_lora_rx_event_callback( envia_temperatura );
     while(1){
-        // lora_event();
+        lora_event();
         if(flag){
           tmr2_set(0);
           modulator_set(false);
           avg_percent = ((240-avg_percent)/115)*100;
-          floatToList(avg_percent, avg_str);
-          envia_humitat();
+          // floatToList(avg_percent, avg_str);
+          // envia_humitat();
+          sensors.requestTemperatures();  // Solicitar la temperatura al sensor
+          float temperatura = sensors.getTempCByIndex(0);  // Obtener la temperatura en grados Celsius
+          // if (temperatura == DEVICE_DISCONNECTED_C) {
+          //   Serial.print("Error Temperatura");
+          // } else {
+          floatToList(avg_percent, temperatura, avg_str);
+          // Serial.println("Bloqueig");
+          envia();
+          // }
         //     // if (estat_reg == parat && avg_percent < llindar_min) {
         //     //     pin_w(REGAR, true);
         //     //     estat_reg = regant;
@@ -203,15 +222,19 @@ ISR(TIMER2_COMPA_vect){
 }   
 
 
-// void parse_lora( uint8_t * buf, uint8_t len, uint8_t status ) {
-// 	if( status == IRQ_PAYLOAD_CRC_ERROR_MASK ) {
-// 		return;
-// 	}
-//     if (buf[0] == 'G' && buf[1] == '3' && buf[2] == ':'){
-//         if(buf[3] == '?' && buf[4] == 'H'){
-//             envia_humitat();
-//         } else if (buf[3] == '?' && buf[4] == 'B'){
-//             envia_estat_bomba();
-//         }
-//     }
-// }
+void parse_lora( uint8_t * buf, uint8_t len, uint8_t status ) {
+  // Serial.println("Re parcero bro");
+	// if( status == IRQ_PAYLOAD_CRC_ERROR_MASK ) {
+	// 	return;
+	// }
+    if (buf[0] == 'G' && buf[1] == '3' && buf[2] == ':'){
+        if(buf[3] == '?' && buf[4] == 'H'){
+          // Serial.println("Recepció petició d'humitat");
+          envia_resposta();
+        } else if (buf[3] == '?' && buf[4] == 'B'){
+            // envia_estat_bomba();
+            // Serial.println("Recepció petició d'estat bomba");
+            // envia();
+        }
+    }
+}
