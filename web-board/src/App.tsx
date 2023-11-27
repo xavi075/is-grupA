ae9da9e66da998ced774f68d4491c4600626e16b
Xavi Massana <xm201532@gmail.com>
2023-11-27 10:01:01 +0100

    Add redirections protections

--- web-board/src/App.tsx
+++ web-board/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import './App.css';
 import { DataPage } from './pages/data/DataPage';
 import { ParametersPage } from './pages/parameters/ParametersPage';
@@ -10,7 +10,8 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { LoginPage } from './pages/login/LoginPage';
 import { ProfilePage } from './pages/profile/ProfilePage';
-// import { UserContextProvider } from './context/UserContext';
+import { useUser } from './context/UserContext';
+
 library.add(fas)
 
 function App() {
@@ -19,17 +20,31 @@ function App() {
     localStorage.clear()
   }, [])
 
+  const { isLoggedIn } = useUser();
+
   return (
       <Router>
         <div className="App">
           <Header/>
+          {isLoggedIn && (
           <Routes>
-            <Route path="/data" element={<DataPage />} />
-            <Route path="/parameters" element={<ParametersPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
+              <Route path="/data" element={<DataPage />} />
+              <Route path="/parameters" element={<ParametersPage />} />
+              <Route path="/home" element={<HomePage />} />
+              <Route path="/" element={<Navigate to="/home"/>} />
+              {/* <Route path="/" redirect="/home" /> */}
+              <Route path="/profile" element={<ProfilePage />} />
           </Routes>
+          )}
+          {!isLoggedIn && (
+          <Routes>           
+              <Route path="/data" element={<Navigate to="/"/>} />
+              <Route path="/parameters" element={<Navigate to="/"/>} />
+              <Route path="/home" element={<Navigate to="/"/>} />
+              <Route path="/" element={<LoginPage />} />
+              <Route path="/profile" element={<Navigate to="/"/>} />
+          </Routes>
+          )}
           <Footer />
         </div>
       </Router>
