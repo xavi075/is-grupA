a3395ce55c0bebba0bbfc0a70a64a359a0dbc3d0
ferriculus <ferrancasanovasp@gmail.com>
2023-12-13 16:52:27 +0100

    primera versió del docker

---
+++ docker/scripts/installDBlibraries.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt-get update
+apt-get -y install python3 
+apt-get -y install python3-pip
+apt-get -y install git
+
+export PATH="$PATH:/usr/local/bin"
+
+pip3 install Flask
+pip3 install pytz
+pip3 install Flask-CORS
+pip3 install mysql-connector-python
